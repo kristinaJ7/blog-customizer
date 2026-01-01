4d4404d19ecf3702de666624e88601fab22c84a2
kristina filimonova <kristina.jk7@yandex.ru>
2026-01-01 17:30:32 +0300

    Обновление проекта, компонент App перенесен  в отдельный файл

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -9,12 +9,13 @@ import {
 	fontColors,
 	backgroundColors,
 	contentWidthArr,
+	defaultArticleState,
 } from 'src/constants/articleProps';
 import { Select } from 'src/ui/select';
 import { Text } from 'src/ui/text';
 import clsx from 'clsx';
-import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
 import { Separator } from 'src/ui/separator';
+import { useClose } from 'src/ui/select/hooks/useClose';
 
 // Тип настроек статьи
 export type ArticleSettings = {
@@ -25,15 +26,6 @@ export type ArticleSettings = {
 	contentWidth: string;
 };
 
-// Начальные значения
-const INITIAL_SETTINGS: ArticleSettings = {
-	font: fontFamilyOptions[0].value,
-	fontSize: fontSizeOptions[0].value,
-	textColor: fontColors[0].value,
-	backgroundColor: backgroundColors[0].value,
-	contentWidth: contentWidthArr[0].value,
-};
-
 // Пропсы компонента
 type ArticleParamsFormProps = {
 	onApply: (settings: ArticleSettings) => void;
@@ -41,148 +33,153 @@ type ArticleParamsFormProps = {
 
 export const ArticleParamsForm = ({ onApply }: ArticleParamsFormProps) => {
 	const [isOpen, setIsOpen] = useState(false);
-	const [settings, setSettings] = useState<ArticleSettings>(INITIAL_SETTINGS);
-	// Реф для отслеживания кликов
-	const formRef = useRef<HTMLDivElement>(null);
 
-	// Хук для закрытия при клике вне формы
-	useOutsideClickClose({
+	// Инициализация состояния через defaultArticleState
+	const [settings, setSettings] = useState<ArticleSettings>({
+		font: defaultArticleState.fontFamilyOption.value,
+		fontSize: defaultArticleState.fontSizeOption.value,
+		textColor: defaultArticleState.fontColor.value,
+		backgroundColor: defaultArticleState.backgroundColor.value,
+		contentWidth: defaultArticleState.contentWidth.value,
+	});
+
+	// Реф для отслеживания кликов
+	const formRef = useRef<HTMLElement>(null);
+
+	// Используем новый хук
+	useClose({
 		isOpen: isOpen,
-		rootRef: formRef,
 		onClose: () => setIsOpen(false),
-		onChange: setIsOpen,
+		rootRef: formRef,
 	});
 
 	// Обработка отправки формы
 	const handleApply = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
+		console.log('Применяем настройки:', settings);
 		onApply(settings); // Передаём настройки в родитель
 		setIsOpen(false); // Добавляем закрытие
 	};
 
-	// Сброс настроек
+	// Сброс настроек — используем defaultArticleState
 	const handleReset = () => {
-		setSettings(INITIAL_SETTINGS);
-		onApply(INITIAL_SETTINGS); // Сразу применяем дефолтные
+		const resetSettings = {
+			font: defaultArticleState.fontFamilyOption.value,
+			fontSize: defaultArticleState.fontSizeOption.value,
+			textColor: defaultArticleState.fontColor.value,
+			backgroundColor: defaultArticleState.backgroundColor.value,
+			contentWidth: defaultArticleState.contentWidth.value,
+		};
+		setSettings(resetSettings);
+		onApply(resetSettings); // Сразу применяем дефолтные
 		setIsOpen(false);
 	};
 
-	//Кнопка открытия/закрытия
+	// Кнопка открытия/закрытия
 	return (
 		<>
 			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen((prev) => !prev)} />
+			<aside
+				className={clsx(styles.container, isOpen && styles.container_open)}
+				ref={formRef}>
+				<form className={styles.form} onSubmit={handleApply}>
+					<Text as='h2' size={31} weight={800} align='left' family='open-sans'>
+						ЗАДАЙТЕ ПАРАМЕТРЫ
+					</Text>
 
-			{isOpen && (
-				<aside //Панель настроек (появляется при isOpen === true)
-					className={clsx(styles.container, isOpen && styles.container_open)}
-					ref={formRef}>
-					<form className={styles.form} onSubmit={handleApply}>
-						<Text
-							as='h2'
-							size={31}
-							weight={800}
-							align='left'
-							family='open-sans'>
-							ЗАДАЙТЕ ПАРАМЕТРЫ
-						</Text>
+					<Select
+						options={fontFamilyOptions}
+						selected={
+							fontFamilyOptions.find(
+								(option) => option.value === settings.font
+							) ?? fontFamilyOptions[0]
+						}
+						onChange={(option) =>
+							setSettings((prev) => ({
+								...prev,
+								font: option.value,
+							}))
+						}
+						title='Шрифт'
+					/>
 
-						<Select //Поля настройки
-							options={fontFamilyOptions}
-							selected={
-								fontFamilyOptions.find(
-									(option) => option.value === settings.font
-								) ?? fontFamilyOptions[0]
-							}
-							onChange={(option) =>
-								setSettings((prev) => ({
-									...prev,
-									font: option.value,
-								}))
-							}
-							title='Шрифт'
+					<RadioGroup
+						name='fontSize'
+						options={fontSizeOptions}
+						selected={
+							fontSizeOptions.find(
+								(option) => option.value === settings.fontSize
+							) ?? fontSizeOptions[0]
+						}
+						onChange={(option) =>
+							setSettings((prev) => ({
+								...prev,
+								fontSize: option.value,
+							}))
+						}
+						title='Размер шрифта'
+					/>
+
+					<Select
+						options={fontColors}
+						selected={
+							fontColors.find(
+								(option) => option.value === settings.textColor
+							) ?? fontColors[0]
+						}
+						onChange={(option) =>
+							setSettings((prev) => ({
+								...prev,
+								textColor: option.value,
+							}))
+						}
+						title='Цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						options={backgroundColors}
+						selected={
+							backgroundColors.find(
+								(option) => option.value === settings.backgroundColor
+							) ?? backgroundColors[0]
+						}
+						onChange={(option) =>
+							setSettings((prev) => ({
+								...prev,
+								backgroundColor: option.value,
+							}))
+						}
+						title='Цвет фона'
+					/>
+
+					<Select
+						options={contentWidthArr}
+						selected={
+							contentWidthArr.find(
+								(option) => option.value === settings.contentWidth
+							) ?? contentWidthArr[0]
+						}
+						onChange={(option) =>
+							setSettings((prev) => ({
+								...prev,
+								contentWidth: option.value,
+							}))
+						}
+						title='Ширина контента'
+					/>
+
+					<div className={styles.bottomContainer}>
+						<Button // Сбросить — вызывает handleReset
+							title='Сбросить'
+							htmlType='button'
+							type='clear'
+							onClick={handleReset}
 						/>
 
-						<RadioGroup
-							name='fontSize'
-							options={fontSizeOptions} //список доступных шрифтов
-							selected={
-								//текущий выбранный шрифт
-								fontSizeOptions.find(
-									(option) => option.value === settings.fontSize
-								) ?? fontSizeOptions[0]
-							}
-							onChange={(
-								option //— обновляет settings при выборе нового значения
-							) =>
-								setSettings((prev) => ({
-									...prev,
-									fontSize: option.value,
-								}))
-							}
-							title='Размер шрифта'
-						/>
-
-						<Select
-							options={fontColors}
-							selected={
-								fontColors.find(
-									(option) => option.value === settings.textColor
-								) ?? fontColors[0]
-							}
-							onChange={(option) =>
-								setSettings((prev) => ({
-									...prev,
-									textColor: option.value,
-								}))
-							}
-							title='Цвет шрифта'
-						/>
-						<Separator />
-						<Select
-							options={backgroundColors}
-							selected={
-								backgroundColors.find(
-									(option) => option.value === settings.backgroundColor
-								) ?? backgroundColors[0]
-							}
-							onChange={(option) =>
-								setSettings((prev) => ({
-									...prev,
-									backgroundColor: option.value,
-								}))
-							}
-							title='Цвет фона'
-						/>
-
-						<Select
-							options={contentWidthArr}
-							selected={
-								contentWidthArr.find(
-									(option) => option.value === settings.contentWidth
-								) ?? contentWidthArr[0]
-							}
-							onChange={(option) =>
-								setSettings((prev) => ({
-									...prev,
-									contentWidth: option.value,
-								}))
-							}
-							title='Ширина контента'
-						/>
-
-						<div className={styles.bottomContainer}>
-							<Button // Сбросить — вызывает handleReset
-								title='Сбросить'
-								htmlType='button'
-								type='clear'
-								onClick={handleReset}
-							/>
-
-							<Button title='Применить' htmlType='submit' type='apply' />
-						</div>
-					</form>
-				</aside>
-			)}
+						<Button title='Применить' htmlType='submit' type='apply' />
+					</div>
+				</form>
+			</aside>
 		</>
 	);
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,57 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState, useEffect } from 'react';
-import clsx from 'clsx';
-
-import { Article } from './components/article/Article';
-import {
-	ArticleParamsForm,
-	ArticleSettings,
-} from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
-
+import { StrictMode } from 'react';
 import './styles/index.scss';
-import styles from './styles/index.module.scss';
+import { App } from './components/App/App';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
-
-const App = () => {
-	// Состояние для хранения текущих настроек статьи
-	const [articleSettings, setArticleSettings] = useState<ArticleSettings>({
-		font: defaultArticleState.fontFamilyOption.value,
-		fontSize: defaultArticleState.fontSizeOption.value,
-		textColor: defaultArticleState.fontColor.value,
-		backgroundColor: defaultArticleState.backgroundColor.value,
-		contentWidth: defaultArticleState.contentWidth.value,
-	});
-
-	// Применяем настройки как CSS-переменные
-	useEffect(() => {
-		const root = document.documentElement;
-		root.style.setProperty('--font-family', articleSettings.font);
-		root.style.setProperty('--font-size', articleSettings.fontSize);
-		root.style.setProperty('--font-color', articleSettings.textColor);
-		root.style.setProperty('--bg-color', articleSettings.backgroundColor);
-		root.style.setProperty('--container-width', articleSettings.contentWidth);
-	}, [articleSettings]);
-
-	return (
-		<main
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': articleSettings.font,
-					'--font-size': articleSettings.fontSize,
-					'--font-color': articleSettings.textColor,
-					'--container-width': articleSettings.contentWidth,
-					'--bg-color': articleSettings.backgroundColor,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm onApply={setArticleSettings} />
-			<Article />
-		</main>
-	);
-};
 
 root.render(
 	<StrictMode>
